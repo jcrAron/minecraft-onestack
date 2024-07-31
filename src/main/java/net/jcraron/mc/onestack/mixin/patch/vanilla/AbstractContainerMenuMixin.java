5187d4bbebff92a3acbb6f592aa14409fb108c6b
aron jcr <leo2317292762@gmail.com>
2024-07-31 22:32:25 +0800

    fix bug: get the correct count
    Getting the correct count when splitting an item in half

--- src/main/java/net/jcraron/mc/onestack/mixin/patch/vanilla/AbstractContainerMenuMixin.java
+++ src/main/java/net/jcraron/mc/onestack/mixin/patch/vanilla/AbstractContainerMenuMixin.java
@@ -1,15 +1,19 @@
 package net.jcraron.mc.onestack.mixin.patch.vanilla;
 
+import java.util.Optional;
 import java.util.Set;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 
 @Mixin(AbstractContainerMenu.class)
 public class AbstractContainerMenuMixin {
@@ -20,5 +24,14 @@ public class AbstractContainerMenuMixin {
 			return;
 		}
 		info.setReturnValue(p_279172_.getCount() / p_279393_.size());
+	}
+
+	@Redirect(method = "doClick(IILnet/minecraft/world/inventory/ClickType;Lnet/minecraft/world/entity/player/Player;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/Slot;tryRemove(IILnet/minecraft/world/entity/player/Player;)Ljava/util/Optional;"))
+	public Optional<ItemStack> doClick(Slot slot, int p_150642_, int p_150643_, Player p_150644_) {
+		int count = slot.getItem().getCount();
+		if (count == Integer.MAX_VALUE && p_150642_ == (count + 1) / 2) {
+			p_150642_ = count / 2 + 1;
+		}
+		return slot.tryRemove(p_150642_, p_150643_, p_150644_);
 	}
 }
