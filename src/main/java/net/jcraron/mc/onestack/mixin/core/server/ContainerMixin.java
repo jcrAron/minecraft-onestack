23b3033d78a932cb656025d441f181165af353c1
aron jcr <leo2317292762@gmail.com>
2024-09-20 23:13:55 +0800

    [2.2.2] add Javadoc on overwritten function

--- src/main/java/net/jcraron/mc/onestack/mixin/core/server/ContainerMixin.java
+++ src/main/java/net/jcraron/mc/onestack/mixin/core/server/ContainerMixin.java
@@ -8,6 +8,10 @@ import net.minecraft.world.Container;
 
 @Mixin(Container.class)
 public interface ContainerMixin {
+	/** 
+	 * @reason overwrite the max size in default. respect instances that override this function
+	 * @author jcraron
+	 * */
 	@Overwrite
 	default int getMaxStackSize() {
 		return MaxCountValue.JAVA_VALUE_MAX;
